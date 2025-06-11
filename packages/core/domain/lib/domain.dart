43aef15501b3bb7333b8cc2542a9fe5ec3b033c4
KsArT-IT <ksart.it@gmail.com>
2025-06-11 15:22:47 +0300

    feat(core_domain): add domain.dart file to export entities and repositories

---
+++ packages/core/domain/lib/domain.dart
@@ -0,0 +1,5 @@
+export 'src/repository/podcast_repository.dart';
+export 'src/entities/episode.dart';
+export 'src/entities/result.dart';
+export 'src/entities/errors.dart';
+export 'src/usecases/fetch_episodes_usecase.dart';
