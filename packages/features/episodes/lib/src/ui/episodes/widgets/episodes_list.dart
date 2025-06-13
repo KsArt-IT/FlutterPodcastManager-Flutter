1ab8b2d2efb77d84e0777bfaeb4f9c555f48fd66
KsArT-IT <ksart.it@gmail.com>
2025-06-13 20:37:49 +0300

    feat(feature_episodes): add the ability to update the episode list using Pull-to-Refresh

--- packages/features/episodes/lib/src/ui/episodes/widgets/episodes_list.dart
+++ packages/features/episodes/lib/src/ui/episodes/widgets/episodes_list.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:core_domain/domain.dart';
+import 'package:feature_episodes/episodes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EpisodesList extends StatelessWidget {
   final List<Episode> list;
@@ -8,20 +12,27 @@ class EpisodesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: list.length,
-      itemBuilder: (context, index) {
-        final item = list[index];
-        return ListTile(
-          key: item.id != null ? ValueKey(item.id) : null,
-          title: Text(item.title),
-          subtitle: Text(item.description),
-          onTap: () {
-            // TODO: open host
-            item.host;
-          },
-        );
+    return RefreshIndicator(
+      onRefresh: () {
+        final completer = Completer<void>();
+        context.read<EpisodesBloc>().add(RefreshEpisodesEvent(completer));
+        return completer.future;
       },
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          final item = list[index];
+          return ListTile(
+            key: item.id.isNotEmpty ? ValueKey(item.id) : null,
+            title: Text(item.title),
+            subtitle: Text(item.description),
+            onTap: () {
+              // TODO: open host
+              item.host;
+            },
+          );
+        },
+      ),
     );
   }
 }
