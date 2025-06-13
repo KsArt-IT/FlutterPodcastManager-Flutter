7338efaba3e51d32b9d3528cf960f2c4c737d4dc
KsArT-IT <ksart.it@gmail.com>
2025-06-13 20:40:39 +0300

    refactor(feature_episodes): update episode edit screen using TextFormFieldValid for input validation

--- packages/features/episodes/lib/src/ui/edit/episode_edit_screen.dart
+++ packages/features/episodes/lib/src/ui/edit/episode_edit_screen.dart
@@ -1,4 +1,5 @@
 import 'package:feature_episodes/episodes.dart';
+import 'package:feature_episodes/src/ui/edit/widgets/text_form_field_valid.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -28,11 +29,12 @@ class _EpisodeEditBody extends StatelessWidget {
     return BlocConsumer<EpisodeEditBloc, EpisodeEditState>(
       listener: (context, state) {
         if (state.status == StateStatus.success) {
-          context.pop();
+          context.pop(state.episode);
         }
       },
       builder: (context, state) {
         final isEdit = state.episode != null;
+        final bloc = context.read<EpisodeEditBloc>();
         return Scaffold(
           appBar: AppBar(title: Text(isEdit ? 'Edit' : 'Create')),
           body: SafeArea(
@@ -40,25 +42,56 @@ class _EpisodeEditBody extends StatelessWidget {
               children: [
                 Expanded(
                   child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        TextField(
-                          decoration: InputDecoration(hintText: 'Title'),
-                          textInputAction: TextInputAction.next,
-                          autocorrect: true,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Form(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextFormFieldValid(
+                              hint: 'Title',
+                              autocorrect: true,
+                              onCheckText: (value) {
+                                return state.status == StateStatus.error &&
+                                        state.error.contains('Title')
+                                    ? state.error
+                                    : null;
+                              },
+                              onChanged: (value) {
+                                bloc.add(ChangeTitleEpisodeEvent(value));
+                              },
+                            ),
+                            // SizedBox(height: 16),
+                            TextFormFieldValid(
+                              hint: 'Description',
+                              autocorrect: true,
+                              onCheckText: (value) {
+                                return state.status == StateStatus.error &&
+                                        state.error.contains('Description')
+                                    ? state.error
+                                    : null;
+                              },
+                              onChanged: (value) {
+                                bloc.add(ChangeDescriptionEpisodeEvent(value));
+                              },
+                            ),
+                            // SizedBox(height: 16),
+                            TextFormFieldValid(
+                              hint: 'Host',
+                              autocorrect: false,
+                              onCheckText: (value) {
+                                return state.status == StateStatus.error &&
+                                        state.error.contains('Host')
+                                    ? state.error
+                                    : null;
+                              },
+                              onChanged: (value) {
+                                bloc.add(ChangeHostEpisodeEvent(value));
+                              },
+                            ),
+                          ],
                         ),
-                        SizedBox(),
-                        TextField(
-                          decoration: InputDecoration(hintText: 'Description'),
-                          textInputAction: TextInputAction.next,
-                          autocorrect: true,
-                        ),
-                        TextField(
-                          decoration: InputDecoration(hintText: 'Host'),
-                          textInputAction: TextInputAction.done,
-                          autocorrect: false,
-                        ),
-                      ],
+                      ),
                     ),
                   ),
                 ),
