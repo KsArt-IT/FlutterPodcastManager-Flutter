b8b196e209bb3b25099640f86c965bf36e0242e6
KsArT-IT <ksart.it@gmail.com>
2025-06-13 15:16:56 +0300

    feat(feature_episodes): update file export for episode editing screen and add routes

--- packages/features/episodes/lib/episodes.dart
+++ packages/features/episodes/lib/episodes.dart
@@ -2,3 +2,4 @@ export 'src/ui/episodes/bloc/episodes_bloc.dart';
 export 'src/ui/episodes/episodes_screen.dart';
 export 'src/ui/edit/bloc/episode_edit_bloc.dart';
 export 'src/ui/edit/episode_edit_screen.dart';
+export 'src/router/episodes_routes.dart';
