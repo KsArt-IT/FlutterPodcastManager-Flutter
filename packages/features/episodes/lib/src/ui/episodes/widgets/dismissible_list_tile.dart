b8fffe6c6b7eb00412338b053a344e1aae2fbaed
KsArT-IT <ksart.it@gmail.com>
2025-06-14 17:56:15 +0300

    feat(feature_episodes): add widget DismissibleListTile to handle swipes in the episode list

---
+++ packages/features/episodes/lib/src/ui/episodes/widgets/dismissible_list_tile.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class DismissibleListTile extends StatelessWidget {
+  final String id;
+  final String title;
+  final String description;
+  final VoidCallback onEdit;
+  final VoidCallback onDelete;
+  final VoidCallback onTap;
+
+  const DismissibleListTile({
+    super.key,
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.onEdit,
+    required this.onDelete,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(id),
+      direction: DismissDirection.horizontal,
+      background: Container(
+        color: Colors.blueAccent,
+        alignment: Alignment.centerLeft,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: const Icon(Icons.edit, color: Colors.white70),
+      ),
+      secondaryBackground: Container(
+        color: Colors.redAccent,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: const Icon(Icons.delete, color: Colors.white70),
+      ),
+      confirmDismiss: (direction) async {
+        if (direction == DismissDirection.startToEnd) {
+          onEdit();
+          return false;
+        } else if (direction == DismissDirection.endToStart) {
+          final confirm = await showDialog<bool>(
+            context: context,
+            builder: (ctx) => AlertDialog.adaptive(
+              title: Text("Delete episode: '$title'?"),
+              actions: [
+                TextButton(
+                  onPressed: () => ctx.pop(false),
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () => ctx.pop(true),
+                  child: const Text('Delete'),
+                ),
+              ],
+            ),
+          );
+          return confirm ?? false;
+        }
+        return false;
+      },
+      onDismissed: (direction) {
+        if (direction == DismissDirection.endToStart) {
+          onDelete();
+        }
+      },
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(description),
+        onTap: onTap,
+      ),
+    );
+  }
+}
