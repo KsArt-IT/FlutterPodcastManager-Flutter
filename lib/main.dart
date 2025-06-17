4e089f2be8b12b0fa0b723d5e067931802489434
KsArT-IT <ksart.it@gmail.com>
2025-06-17 23:58:11 +0300

    feat(main): add loading of environment variables from .env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_podcast_manager/app/app.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  
   runApp(const App());
 }
