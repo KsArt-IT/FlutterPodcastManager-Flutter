d1a6da54d7622e00485fa696a5d37586e81de834
KsArT-IT <ksart.it@gmail.com>
2025-06-18 11:54:02 +0300

    feat(core_domain): update export to add GenerateEpisodeUseCase and LlmGenerateRepository

--- packages/core/domain/lib/domain.dart
+++ packages/core/domain/lib/domain.dart
@@ -1,9 +1,11 @@
 export 'src/entities/episode.dart';
 export 'src/entities/errors.dart';
 export 'src/entities/result.dart';
+export 'src/repository/llm_generate_repository.dart';
 export 'src/repository/podcast_repository.dart';
 export 'src/usecases/create_episode_usecase.dart';
 export 'src/usecases/delete_episode_usecase.dart';
 export 'src/usecases/fetch_episode_usecase.dart';
 export 'src/usecases/fetch_episodes_usecase.dart';
+export 'src/usecases/generate_episode_usecase.dart';
 export 'src/usecases/update_episode_usecase.dart';
