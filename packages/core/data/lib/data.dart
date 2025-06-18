1005d7a99d00361ca0d854a07fe301d4bd781f92
KsArT-IT <ksart.it@gmail.com>
2025-06-18 11:51:49 +0300

    feat(core_data): add export of DeepSeek API services to data.dart

--- packages/core/data/lib/data.dart
+++ packages/core/data/lib/data.dart
@@ -1,3 +1,5 @@
 export 'src/repository/podcast_repository_impl.dart';
+export 'src/repository/deepseek_generate_repository_impl.dart';
 export 'src/datasources/podcast_api_service.dart';
+export 'src/datasources/deepseek/deepseek_api_service.dart';
 export 'package:dio/dio.dart';
