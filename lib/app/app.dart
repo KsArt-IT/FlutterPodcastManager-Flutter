8594d70f628267813bdb34f06e13139a7027d97a
KsArT-IT <ksart.it@gmail.com>
2025-06-18 13:51:36 +0300

    feat: add DeepSeek API for episode text generation

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:core_data/data.dart';
 import 'package:core_domain/domain.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_podcast_manager/app/podcast_manager_app.dart';
 
 class App extends StatelessWidget {
@@ -9,13 +10,24 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+    dio.options.headers["content-type"] = "application/json";
     return MultiRepositoryProvider(
       providers: [
         RepositoryProvider<PodcastApiService>(
-          create: (_) => PodcastApiService(Dio()),
+          create: (_) => PodcastApiService(dio),
         ),
         RepositoryProvider<PodcastRepository>(
           create: (context) => PodcastRepositoryImpl(service: context.read()),
+        ),
+        RepositoryProvider<DeepSeekApiService>(
+          create: (_) => DeepSeekApiService(dio),
+        ),
+        RepositoryProvider<LlmGenerateRepository>(
+          create: (context) => DeepSeekGenerateRepositoryImpl(
+            service: context.read(),
+            apiKey: dotenv.env['DEESEEK_API_KEY'] ?? '',
+          ),
         ),
         RepositoryProvider<FetchEpisodesUseCase>(
           create: (context) => FetchEpisodesUseCase(context.read()),
@@ -31,6 +43,9 @@ class App extends StatelessWidget {
         ),
         RepositoryProvider<DeleteEpisodeUseCase>(
           create: (context) => DeleteEpisodeUseCase(context.read()),
+        ),
+        RepositoryProvider<GenerateEpisodeUseCase>(
+          create: (context) => GenerateEpisodeUseCase(context.read()),
         ),
       ],
       child: PodcastManagerApp(),
