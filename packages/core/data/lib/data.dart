345ef7d2d51563ed447613f852c10dccc2a65b83
KsArT-IT <ksart.it@gmail.com>
2025-06-18 18:59:56 +0300

    feat(core_data): update exported modules and add new ones to data.dart

--- packages/core/data/lib/data.dart
+++ packages/core/data/lib/data.dart
@@ -1,5 +1,11 @@
-export 'src/repository/podcast_repository_impl.dart';
-export 'src/repository/deepseek_generate_repository_impl.dart';
-export 'src/datasources/podcast_api_service.dart';
-export 'src/datasources/deepseek/deepseek_api_service.dart';
 export 'package:dio/dio.dart';
+
+export 'src/datasources/api_service.dart';
+export 'src/datasources/deepseek/deepseek_api_service.dart';
+export 'src/datasources/huggingface/huggingface_api_service.dart';
+export 'src/datasources/podcast_api_service.dart';
+export 'src/models/deepseek/deepseek_response.dart';
+export 'src/models/huggingface/hugging_face_response.dart';
+export 'src/repository/deepseek_generate_repository_impl.dart';
+export 'src/repository/huggingface_generate_repository_impl.dart';
+export 'src/repository/podcast_repository_impl.dart';
