f1109770bcdd4e6550eb95d0d24a2a671a1a4915
KsArT-IT <ksart.it@gmail.com>
2025-06-20 16:46:48 +0300

    refactor(feature_episodes): change episode editing screen with added width constraints and centering

--- packages/features/episodes/lib/src/ui/edit/episode_edit_screen.dart
+++ packages/features/episodes/lib/src/ui/edit/episode_edit_screen.dart
@@ -33,107 +33,120 @@ class _EpisodeEditBody extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(isEdit ? 'Edit' : 'Create')),
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Form(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        BlocBuilder<EpisodeEditBloc, EpisodeEditState>(
-                          buildWhen: (previous, current) =>
-                              previous.title != current.title,
-                          builder: (context, state) {
-                            return TextFormFieldValid(
-                              value: state.title,
-                              hint: 'Title',
-                              autocorrect: true,
-                              onCheckText: (value) {
-                                return state.status == StateStatus.error &&
-                                        state.error.contains('Title')
-                                    ? state.error
-                                    : null;
+        child: Center(
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: 600),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(16, 32, 16, 0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.grey[300],
+                      ),
+                      child: Form(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            BlocBuilder<EpisodeEditBloc, EpisodeEditState>(
+                              buildWhen: (previous, current) =>
+                                  previous.title != current.title,
+                              builder: (context, state) {
+                                return TextFormFieldValid(
+                                  value: state.title,
+                                  hint: 'Title',
+                                  autocorrect: true,
+                                  onCheckText: (value) {
+                                    return state.status == StateStatus.error &&
+                                            state.error.contains('Title')
+                                        ? state.error
+                                        : null;
+                                  },
+                                  onChanged: (value) => context
+                                      .read<EpisodeEditBloc>()
+                                      .add(ChangeTitleEpisodeEvent(value)),
+                                );
                               },
-                              onChanged: (value) => context
-                                  .read<EpisodeEditBloc>()
-                                  .add(ChangeTitleEpisodeEvent(value)),
-                            );
-                          },
-                        ),
-                        BlocBuilder<EpisodeEditBloc, EpisodeEditState>(
-                          buildWhen: (previous, current) =>
-                              previous.description != current.description,
-                          builder: (context, state) {
-                            return TextFormFieldValid(
-                              value: state.description,
-                              hint: 'Description',
-                              autocorrect: true,
-                              onCheckText: (value) {
-                                return state.status == StateStatus.error &&
-                                        state.error.contains('Description')
-                                    ? state.error
-                                    : null;
+                            ),
+                            BlocBuilder<EpisodeEditBloc, EpisodeEditState>(
+                              buildWhen: (previous, current) =>
+                                  previous.description != current.description,
+                              builder: (context, state) {
+                                return TextFormFieldValid(
+                                  value: state.description,
+                                  hint: 'Description',
+                                  autocorrect: true,
+                                  onCheckText: (value) {
+                                    return state.status == StateStatus.error &&
+                                            state.error.contains('Description')
+                                        ? state.error
+                                        : null;
+                                  },
+                                  onChanged: (value) =>
+                                      context.read<EpisodeEditBloc>().add(
+                                        ChangeDescriptionEpisodeEvent(value),
+                                      ),
+                                );
                               },
-                              onChanged: (value) => context
-                                  .read<EpisodeEditBloc>()
-                                  .add(ChangeDescriptionEpisodeEvent(value)),
-                            );
-                          },
-                        ),
-                        BlocBuilder<EpisodeEditBloc, EpisodeEditState>(
-                          buildWhen: (previous, current) =>
-                              previous.host != current.host,
-                          builder: (context, state) {
-                            return TextFormFieldValid(
-                              value: state.host,
-                              hint: 'Host',
-                              autocorrect: false,
-                              onCheckText: (value) {
-                                return state.status == StateStatus.error &&
-                                        state.error.contains('Host')
-                                    ? state.error
-                                    : null;
+                            ),
+                            BlocBuilder<EpisodeEditBloc, EpisodeEditState>(
+                              buildWhen: (previous, current) =>
+                                  previous.host != current.host,
+                              builder: (context, state) {
+                                return TextFormFieldValid(
+                                  value: state.host,
+                                  hint: 'Host',
+                                  autocorrect: false,
+                                  onCheckText: (value) {
+                                    return state.status == StateStatus.error &&
+                                            state.error.contains('Host')
+                                        ? state.error
+                                        : null;
+                                  },
+                                  onChanged: (value) => context
+                                      .read<EpisodeEditBloc>()
+                                      .add(ChangeHostEpisodeEvent(value)),
+                                );
                               },
-                              onChanged: (value) => context
-                                  .read<EpisodeEditBloc>()
-                                  .add(ChangeHostEpisodeEvent(value)),
-                            );
-                          },
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                    const SizedBox(height: 24),
+                    BlocConsumer<EpisodeEditBloc, EpisodeEditState>(
+                      listener: (context, state) {
+                        if (state.status == StateStatus.success) {
+                          context.pop(state.episode);
+                        }
+                      },
+                      buildWhen: (previous, current) =>
+                          previous.status != current.status,
+                      builder: (context, state) {
+                        return SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: state.status == StateStatus.valid
+                                ? () {
+                                    context.read<EpisodeEditBloc>().add(
+                                      SaveEpisodeEvent(),
+                                    );
+                                  }
+                                : null,
+                            child: Text(isEdit ? 'Update' : 'Create'),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: BlocConsumer<EpisodeEditBloc, EpisodeEditState>(
-                listener: (context, state) {
-                  if (state.status == StateStatus.success) {
-                    context.pop(state.episode);
-                  }
-                },
-                buildWhen: (previous, current) =>
-                    previous.status != current.status,
-                builder: (context, state) {
-                  return ElevatedButton(
-                    onPressed: state.status == StateStatus.valid
-                        ? () {
-                            context.read<EpisodeEditBloc>().add(
-                              SaveEpisodeEvent(),
-                            );
-                          }
-                        : null,
-                    child: Text(isEdit ? 'Update' : 'Create'),
-                  );
-                },
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
