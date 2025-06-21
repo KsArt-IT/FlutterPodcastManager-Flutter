18e2664ec59d3654415c51d680c9c6d7b99f95d4
KsArT-IT <ksart.it@gmail.com>
2025-06-21 16:56:33 +0300

    feat(feature_episodes): extend GenerateTextEvent to support new events and Equatable

--- packages/features/episodes/lib/src/ui/generate/bloc/generate_text_event.dart
+++ packages/features/episodes/lib/src/ui/generate/bloc/generate_text_event.dart
@@ -1,17 +1,35 @@
 part of 'generate_text_bloc.dart';
 
-sealed class GenerateTextEvent {}
+sealed class GenerateTextEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 final class GenerateStringEvent extends GenerateTextEvent {}
+
+final class ApplyEpisodeEvent extends GenerateTextEvent {}
 
 final class ChangePromptEvent extends GenerateTextEvent {
   final String prompt;
 
   ChangePromptEvent(this.prompt);
+
+  @override
+  List<Object?> get props => [prompt];
+}
+
+final class ChangeTextEvent extends GenerateTextEvent {
+  final String text;
+
+  ChangeTextEvent(this.text);
+  @override
+  List<Object?> get props => [text];
 }
 
 final class SelectTargetEvent extends GenerateTextEvent {
   final EpisodeTarget target;
 
   SelectTargetEvent(this.target);
+  @override
+  List<Object?> get props => [target];
 }
